bdbfd2de5182f127bcb7e11541aa5c6f677ab255
Evangelin-dev <tech@thebot.agency>
2025-07-10 00:25:23 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,16 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  async redirects() {
+    return [
+      {
+        source: '/(.*)',
+        has: [{ type: 'protocol', value: 'http' }],
+        destination: 'https://www.uyirihealing.com/:path*',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 export default nextConfig
